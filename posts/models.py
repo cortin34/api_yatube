5fda1fa0f1f988a6931a490d989ac11d135ac53c
yandex-praktikum <58176914+yandex-praktikum@users.noreply.github.com>
2020-11-02 18:38:47 +0300

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -6,8 +6,8 @@ User = get_user_model()
 
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField("Дата публикации", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(upload_to='posts/', null=True, blank=True)  # поле для картинки
 
     def __str__(self):
@@ -15,7 +15,7 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     text = models.TextField()
-    created = models.DateTimeField("Дата добавления", auto_now_add=True, db_index=True)
+    created = models.DateTimeField('Дата добавления', auto_now_add=True, db_index=True)
